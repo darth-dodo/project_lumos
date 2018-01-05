78f14690c59342c0026aa85c797530ee5a633548
Abhishek Juneja <abhishek.juneja145@gmail.com>
2018-01-05 14:01:47 +0530

    collecting static

--- static/js/technical_skills_landing.js
+++ static/js/technical_skills_landing.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-    
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-86912923-1', 'auto');
+      ga('send', 'pageview');
+
+      ga('require', 'ec');
+
     $(".skill").hover(function () {
         // $(this).transition("bounce");
         $(this).toggleClass("inverted red")
@@ -10,9 +20,20 @@ $(document).ready(function(){
         var url_arr = location_url.split("/");
         // url is "/http://projectlumos.pythonanywhere.com/technical/knowledge-base/"
         var location_slug = url_arr[url_arr.length - 2]
-        console.log(location_slug);
         window.open('/csv_gen/'+location_slug,'_blank');
-        // fetchCsv(location_slug);
     });
 
+    $(".skill").click(function(){
+            var skill = $(this).text()
+            ga('ec:addProduct', {
+              'id': 'P12345',
+              'name': skill
+              'category': 'Apparel',
+              'brand': 'Google',
+              'variant': 'black'
+            });
+
+            ga('ec:setAction', 'detail');
+            }
+        )
 });//document ready
