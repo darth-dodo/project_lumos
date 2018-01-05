2b84ab0ee787474e97df4749b7b14edf6815ccb9
Abhishek Juneja <abhishek.juneja145@gmail.com>
2018-01-05 14:00:50 +0530

    enhanced_ecomm

--- lumos/static/js/technical_skills_landing.js
+++ lumos/static/js/technical_skills_landing.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-86912923-1', 'auto');
+      ga('send', 'pageview');
+
+      ga('require', 'ec');
+
     $(".skill").hover(function () {
         // $(this).transition("bounce");
         $(this).toggleClass("inverted red")
@@ -13,4 +23,17 @@ $(document).ready(function(){
         window.open('/csv_gen/'+location_slug,'_blank');
     });
 
+    $(".skill").click(function(){
+            var skill = $(this).text()
+            ga('ec:addProduct', {
+              'id': 'P12345',
+              'name': skill
+              'category': 'Apparel',
+              'brand': 'Google',
+              'variant': 'black'
+            });
+
+            ga('ec:setAction', 'detail');
+            }
+        )
 });//document ready
